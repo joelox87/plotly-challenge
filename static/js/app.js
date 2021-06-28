5d7404903f3e5d24e54da6b07e18bf979d3c2e94
joelox87 <j.aponte87@gmail.com>
2021-06-28 10:44:09 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,17 @@
-// Use the D3 library to read in samples.json.
+// Read samples.json file using D3
 d3.json("samples.json").then(function createPlotly(data) {
     console.log(data);
     var testid = data.names;
     console.log(testid);
   
+ // Dropdown menu Setup
+ d3.select("#selDataset")
+ .selectAll("option")
+ .data(testid)
+ .enter()
+ .append("option")
+ .html(function(d) {
+   return `<option>${d}</option`;
+ });
+ 
+ 
