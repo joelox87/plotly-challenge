037c8c7ebd506a5b42c4b360eb904ed08ea663aa
joelox87 <j.aponte87@gmail.com>
2021-06-28 10:50:03 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -19,5 +19,44 @@ var dropdownMenu = d3.select("#selDataset");
 var dropdownValue = dropdownMenu.property("value");
 var index = testid.indexOf(dropdownValue);
 
+// Demographic information
+d3.select("#sample-metadata").html("");
+d3.select("#sample-metadata")
+.selectAll("p")
+.data(Object.entries(data.metadata[index]))
+.enter()
+.append("p")
+.html(function(d) {
+ return `${d[0]} : ${d[1]}`;
+});
+
+console.log(Object.entries(data.metadata[index]));
+
+// Bar graph setup
+var defaultsampleData = data.samples[index].sample_values
+.slice(0, 10)
+.reverse();
+var defaultotuData = data.samples[index].otu_ids.slice(0, 10).reverse();
+var defaultotuLabels = data.samples[index].otu_labels.slice(0, 10).reverse();
+var defaultyxis = defaultotuData.map(a => "OTU" + a);
+
+var bardata = [
+{
+ x: defaultsampleData,
+ y: defaultyxis,
+ type: "bar",
+ orientation: "h",
+ text: defaultotuLabels
+}
+];
+
+var barLayout = {
+title: "TOP10 Sample Values",
+xaxis: { title: "Sample Values" },
+yaxis: { title: "OTU ID" }
+};
+
+Plotly.newPlot("bar", bardata, barLayout);
+
 
 });
