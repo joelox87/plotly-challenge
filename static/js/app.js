aefefe564355487f947f79d57af7d8356c8f4058
joelox87 <j.aponte87@gmail.com>
2021-06-28 18:40:14 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -146,5 +146,18 @@ var bubbleData = [
             ]
           },
          
-                          
+          hole: 0.5,
+          type: "pie",
+          direction: "clockwise",
+          rotation: 90,
+          text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+          textinfo: "text",
+          textposition: "inside",
+          hoverinfo: "none",
+          showlegend: false
+         }
+         ];
+         
+         
+                         
 });
